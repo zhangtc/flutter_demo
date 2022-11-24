83a727b9f6a838271cf05c6e52df3c6f9e6a1abf
tiancheng.zhang <tiancheng.zhang@g42.ai>
2022-11-24 18:02:28 +0400

    commit

---
+++ lib/myIcon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyIcon {
+  static const IconData book =
+      IconData(0x3447, fontFamily: "myIcons", matchTextDirection: true);
+
+  static const IconData weiXin =
+      IconData(0xe893, fontFamily: "myIcons", matchTextDirection: true);
+
+  static const IconData gouWuCheMan =
+      IconData(0x344b, fontFamily: "myIcons", matchTextDirection: true);
+
+  static const IconData fenlei =
+      IconData(0x100d9, fontFamily: "myIcons2", matchTextDirection: true);
+
+  static const IconData shuqian =
+      IconData(0x100da, fontFamily: "myIcons2", matchTextDirection: true);
+
+  static const IconData bianji =
+      IconData(0x100db, fontFamily: "myIcons2", matchTextDirection: true);
+}
